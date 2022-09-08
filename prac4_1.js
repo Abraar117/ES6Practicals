27fe9770cd2475527f65af590818111390753338
Abraar117 <kaziabraar@gmail.com>
2022-09-08 11:58:11 +0530

    new

---
+++ prac4_1.js
@@ -0,0 +1,13 @@
+//var variable
+var greetings = "hello world";
+greetings = "hello this a new greeting";
+console.log(greetings)
+
+//let variable
+let car = "mercedes";
+car = "audi";
+console.log(car)
+
+//const variable
+const watch = "skagen";
+console.log(watch)
