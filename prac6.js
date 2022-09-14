2c9f31a18f2098e0d45ac6c88c8d893ab3cef358
Abraar117 <kaziabraar@gmail.com>
2022-09-15 00:55:32 +0530

    functions

--- prac6.js
+++ prac6.js
@@ -5,18 +5,43 @@ function Car() {
         console.log(a[i]);
     }
 }
-console.log("Result from Normal Function: ",Car())
+console.log("Result from Normal Function: ")
+Car();
 
 //Arrow Function
+console.log("\n")
 let sum = (a,b) => {
     let result = a + b;
     return result;
 }
 let result1 = sum(8,4);
-console.log("Result from Arrow Function: ",result1)
+console.log("Result from Arrow Function: ",result1);
+console.log("\n");
 
 //Generator Function
+function * generatorFunction() {
+    console.log("Result from Generator function: ");
+    console.log("This is the statement before yield.");
+    yield "Hello";
 
+    console.log("This is the statement after first yield.");
+    yield "World";
 
+    console.log("This is the statement after second yield.");
+}
+const generatorObject = generatorFunction();
+
+console.log(generatorObject.next().value);
+console.log(generatorObject.next().value);
+console.log(generatorObject.next().value);
 
 //Anonymous Function
+let anonymous = {
+    first: 'Anonymous',
+    last: 'Function'
+};
+
+(function () {
+    console.log("\nResult from Anonymous Function: ")
+    console.log(anonymous.first + ' ' + anonymous.last);
+})(anonymous);
